d0e0c56496fd6fb5b0046b5048b273c49a938674
Nipun Yasas <bandaraopnyk.22@uom.lk>
2026-07-06 09:10:03 +0530

    feat: initialize Spring Boot backend project with Maven wrapper and standard project structure

---
+++ src/main/java/com/bandits/bhumisaara/BhumisaaraApplication.java
@@ -0,0 +1,13 @@
+package com.bandits.bhumisaara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhumisaaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhumisaaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandits/bhumisaara/BhumisaaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandits.bhumisaara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhumisaaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
